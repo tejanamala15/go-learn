b8bbed07c9a0967293615e56f8f026db4d8ad31e
Namala Teja Reddy <tejanamala15@gmail.com>
2023-10-31 23:54:13 +0530

    go learn day 2

--- main.go
+++ main.go
@@ -15,6 +15,14 @@ func main(){
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+
+    //array
+	// var bookings [50] 
+	
+	//slice
+	var bookings [] string
+	
+
 	var firstName string
 	var lastName string
 	var email string
@@ -31,7 +39,20 @@ func main(){
 	fmt.Println("Please enter the number of tickers you need:")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - uint(userTickets)
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName + " " + lastName)
+
+	fmt.Printf("The whole slice: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Slice length: %v\n", len(bookings))
+
+
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings %v\n", bookings)
 
 
 }
